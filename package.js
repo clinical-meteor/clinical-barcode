fbe4784475bc849c9730c893cdc7896f40dea070
Abigail Watson <awatson1978@gmail.com>
2015-10-18 20:52:39 -0700

    addAssets

--- package.js
+++ package.js
@@ -1,11 +1,11 @@
 Package.describe({
   summary: 'Provides the 3 of 9 Barcode font.',
-  version: '3.0.0',
+  version: '3.0.1',
   name: 'clinical:barcode',
   git: 'https://github.com/clinical-meteor/clinical-barcode'
 });
 
 Package.on_use(function (api) {
-  api.addFiles('fonts/3OF9_NEW.TTF', 'client');
-  api.addFiles('fonts-barcode.css', 'client');
+  api.addAssets('fonts/3OF9_NEW.TTF', 'client');
+  api.addAssets('fonts-barcode.css', 'client');
 });
